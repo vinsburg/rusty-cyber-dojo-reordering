58ba8fa452f3bb01ac2c87e3162505dcd3680242
vinsburg <vinsburg@gmail.com>
2023-11-04 11:01:43 +0200

    return empty slice for indentical start and end indices

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // #![cfg_attr(feature = "strict", deny(warnings))]
 
-pub fn get_sub_sequence(sequence: &Vec<i32>, start: i32, end:i32) -> &Vec<i32> {
-    return sequence;
+pub fn get_sub_sequence(seq: &Vec<i32>, start: usize, end:usize) -> &[i32] {
+    &seq[start..end]
 }
