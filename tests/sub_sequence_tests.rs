02b2d285382365999426ff54d7cf783e08897c48
vinsburg <vinsburg@gmail.com>
2023-11-05 08:08:08 +0200

    Added test for slice from beginning

--- tests/sub_sequence_tests.rs
+++ tests/sub_sequence_tests.rs
@@ -9,3 +9,10 @@ fn should_return_empty_sub_sequence() {
     let expected: Vec<i32> = vec![];
     assert_eq!(&expected, reordering::get_sub_sequence(&seq, 0, 0));
 }
+
+#[test]
+fn should_get_slice_from_beginning() {
+    let seq: Vec<i32> = vec![1,2,3,4,5];
+    let expected: Vec<i32> = vec![1];
+    assert_eq!(&expected, reordering::get_sub_sequence(&seq, 0, 1));
+}
