89dd0a3cbc53184ec519f44134eb6d048b755b41
vinsburg <vinsburg@gmail.com>
2023-11-06 07:25:21 +0200

    Added reorder method and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
 mod slice;
+
+#[allow(dead_code)]
+fn reorder(seq: &mut Vec<i32>, start: usize, end: usize, dest: usize) {
+    println!("reorder: {:?}, {}, {}, {}", seq, start, end, dest);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::reorder;
+    use std::vec::Vec;
+
+    #[test]
+    fn should_reorder_empty_sub_sequence() {
+        let mut seq: Vec<i32> = vec![1, 2];
+        let expected: Vec<i32> = vec![1, 2];
+        reorder(&mut seq, 0, 0, 0);
+        assert_eq!(expected, seq);
+    }
+}
