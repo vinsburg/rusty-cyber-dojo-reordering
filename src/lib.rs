4d82b8acda5a54f0d3630ce579d71c5f98765d55
vinsburg <vinsburg@gmail.com>
2024-02-16 19:12:22 +0200

    Can now move middle slice to beginning

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,24 @@ mod slice;
 
 #[allow(dead_code)]
 fn reorder(seq: &mut Vec<i32>, start: usize, end: usize, dest: usize) {
-    println!("reorder: {:?}, {}, {}, {}", seq, start, end, dest);
     let selected_seq: Vec<i32> = slice::get_slice(seq, start, end);
     let before_seq: Vec<i32> = slice::get_slice(seq, 0, start);
     let after_seq: Vec<i32> = slice::get_slice(seq, end, seq.len());
     let remaining_seq: Vec<i32> = [before_seq, after_seq].concat();
-    let result: Vec<i32> = [remaining_seq, selected_seq].concat();
+    let new_dest: usize = adjust_destination_index(dest, start, end);
+    let new_before_seq: Vec<i32> = slice::get_slice(&remaining_seq, 0, new_dest);
+    let new_after_seq: Vec<i32> = slice::get_slice(&remaining_seq, new_dest, remaining_seq.len());
+    let result: Vec<i32> = [new_before_seq, selected_seq, new_after_seq].concat();
     seq.clear();
     seq.extend(result);
+}
+
+fn adjust_destination_index(dest: usize, start: usize, end: usize) -> usize {
+    if dest > end {
+        dest - end + start
+    } else {
+        dest
+    }
 }
 
 #[cfg(test)]
